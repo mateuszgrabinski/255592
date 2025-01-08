5275046d776878f9971dd79c7bd337e5d039504f
mateuszgrabinski <255592@edu.p.lodz.pl>
2025-01-08 12:54:45 +0100

    01 funkcja policz

--- main.c
+++ main.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
-int policz(int a, int b) {
-    return a+b;
-}
 int main(void) {
     printf("Hello, World!\n");
-    printf("W pieryszym branchu: %d\n", policz(5, 10));
     return 0;
 }
